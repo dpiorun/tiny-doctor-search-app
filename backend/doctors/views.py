4bbab4e45c33c67a15191e1d1cd578b1dbd3dc66
Damian Piorun <piorun@bakboka.com>
2022-10-07 00:26:50 +0200

    backend: add doctors view

--- backend/doctors/views.py
+++ backend/doctors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from doctors.models import Doctor
+from doctors.serializers import DoctorsSerializer
+
+
+class DoctorView(ListAPIView):
+    """
+    Get a list of doctors.
+    """
+
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorsSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["city", "facility", "area_of_expertise"]
